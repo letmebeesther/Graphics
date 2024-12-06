f74009f77f1358e948f643fcf2f95d3e514b1801
Esther Jheon <95136314+letmebeesther@users.noreply.github.com>
2024-12-06 13:43:05 +0900

    Add files via upload

---
+++ 그래픽스 기말고사/그래픽스 기말고사/그래픽스 기말고사.cpp
@@ -0,0 +1,89 @@
+﻿#include <windows.h>
+#include <glut.h>
+#include <math.h>
+#define GL_PI 3.1415f
+static GLfloat xRot = 0.0f;
+static GLfloat yRot = 0.0f;
+
+void SetupRC()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glFrontFace(GL_CCW);
+	glEnable(GL_CULL_FACE);
+}
+
+void TimerFunc(int value)
+{
+	glutPostRedisplay();
+	glutTimerFunc(100, TimerFunc, 1);
+}
+
+void ChangeSize(int w, int h)
+{
+	GLfloat nRange = 150.0f;
+	glViewport(0, 0, w, h);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	if (w <= h)
+		glOrtho(-nRange, nRange, -nRange * h / w, nRange * h / w, -nRange * 2.0f, nRange * 2.0f);               //add
+	else
+		glOrtho(-nRange * w / h, nRange * w / h, -nRange, nRange, -nRange * 2.0f, nRange * 2.0f);  //add
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+
+void RenderScene()
+{
+	static GLfloat fElect1 = 0.0f;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, -100.0f);
+	glColor3ub(255, 0, 0);
+	glutSolidSphere(10.0f, 15, 15);
+
+	glPushMatrix();
+	glRotatef(fElect1, 0.0f, 1.0f, 0.0f);  
+	glTranslatef(90.0f, 0.0f, 0.0f);
+	glColor3ub(0, 0, 255);
+	glutSolidSphere(6.0f, 15, 15);
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
+	glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+	glTranslatef(-70.0f, 0.0f, 0.0f);
+	glutSolidSphere(6.0f, 15, 15);
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef(90.0f, 0.0f, 0.0f, 1.0f);
+	glRotatef(fElect1, 0.0f, 1.0f, 0.0f);
+	glTranslatef(0.0f, 0.0f, 60.0f);
+	glutSolidSphere(6.0f, 15, 15);
+	glPopMatrix();
+
+	fElect1 += 10.0f;
+	if (fElect1 > 360.0f)
+		fElect1 = 0.0f;
+
+	glutSwapBuffers();
+}
+
+void main(void)
+{
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 800);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Solar");
+	glutReshapeFunc(ChangeSize);
+	glutTimerFunc(33, TimerFunc, 1);
+	glutDisplayFunc(RenderScene);
+	SetupRC();
+	glutMainLoop();
+}
